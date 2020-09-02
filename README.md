32ddb58dc7b276b774d5ae3f021f693e0f4ef185
Dominik Geng <domske@mailbox.org>
2020-09-02 02:41:03 +0200

    Light mode and more demos.

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Some examples for the web component [ui-progress-circle][package]
 
-![](img/demo.gif)
+![](img/demo-1.gif)
+![](img/demo-2.gif)
+![](img/demo-3.png)
+![](img/demo-4.gif)
 
 [package]: https://www.npmjs.com/package/ui-progress-circle
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'progress-circle-demo';
   gaugeValue = 33;
+
+  ngOnInit() {
+    const light = window.localStorage.getItem('light');
+    if (light && light === 'true') {
+      document.body.classList.add('light');
+    }
+  }
+
+  toggleLightMode() {
+    document.body.classList.toggle('light');
+    window.localStorage.setItem(
+      'light',
+      `${document.body.classList.contains('light')}`
+    );
+  }
 }
